2b641a3dd58d58bb9285eb73c70788863f80ecf9
gusdn7142 <gusdnchl7144@naver.com>
2022-07-11 17:34:53 +0900

    Bean Validation Study
    Bean Validation (FieldError, ObjectError)

--- spring mvc2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ spring mvc2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,14 +1,32 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총 합이 10000원 넘게 입력해 주세요.")   //가격 * 수량이 10000원을 넘어야 한다.
 public class Item {
 
     private Long id;
+
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
+
+
+
 
     public Item() {
     }
@@ -18,4 +36,7 @@ public class Item {
         this.price = price;
         this.quantity = quantity;
     }
+
+
+
 }
