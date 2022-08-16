7ba68f5bf2ca991cb055725d8c9d662a7313c3ce
gusdn7142 <gusdnchl7144@naver.com>
2022-08-16 22:37:33 +0900

    test

---
+++ jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,18 @@
+package hellojpa;
+
+//import javax.pe
+
+
+public class JpaMain {
+
+    //Persistence
+    public static void main(String[] args) {
+
+        System.out.println("실행");
+
+
+
+    }
+
+
+}
