2d3cdba3b800c938e6047622052518dad863a0f8
gusdn7142 <gusdnchl7144@naver.com>
2022-09-05 15:53:32 +0900

    Jpa Basic Study
    JPQL - Fetch Join

--- jpql/src/main/java/Jpql/JpaMain.java
+++ jpql/src/main/java/Jpql/JpaMain.java
@@ -3,6 +3,7 @@ package Jpql;
 import com.sun.org.apache.xpath.internal.operations.Or;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -20,33 +21,53 @@ public class JpaMain {
         tx.begin();  // 트랜잭션 시작
 
         try {
-                Team team = new Team();
-                team.setName("teamA");
-                em.persist(team);
 
-                Member member = new Member();
-                member.setUsername("member1");
-                //member.setAge(10);
-                //member.setTeam(team);
-                //member.setType(MemberType.ADMIN);
-                em.persist(member);
+            Team teamA = new Team();
+            teamA.setName("teamA");
+            em.persist(teamA);
 
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
 
-                Member member2 = new Member();
-                member2.setUsername("member2");
-                em.persist(member2);
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            member1.setTeam(teamA);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("member2");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
 
             em.flush();
             em.clear();
 
-            String query = "select function('group_concat',m.username) from Member m";   //group_concat(m.username) 도가능
-            List<String> result = em.createQuery(query,String.class)
+            String query = "select m from Member m join fetch m.team";
+            List<Member> result = em.createQuery(query,Member.class)
                     .getResultList();
 
-            for (String s : result) {
-                System.out.println("s = " + s);
+            for (Member member : result) {
+                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
             }
+
+
+//            String query = "select t from Team t join fetch t.members";
+//            List<Team> result = em.createQuery(query,Team.class)
+//                    .getResultList();
+//
+//            for (Team team : result) {
+//                System.out.println("team = " + team.getName() + "|" + team.getMembers().size());
+//                for(Member member : team.getMembers()){
+//                    System.out.println("member = " + member);
+//                }
+//            }
 
 
 
