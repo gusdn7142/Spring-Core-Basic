4f8944f4f02e5c55abc0e16b9ffd2d964a372012
gusdn7142 <gusdnchl7144@naver.com>
2022-09-22 18:44:54 +0900

    Querydsl Study
    Spring Data JPA Test / Custom Repository Test

---
+++ queridsl/src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
@@ -0,0 +1,12 @@
+package study.querydsl.repository;
+
+import study.querydsl.dto.MemberSearchParam;
+import study.querydsl.dto.MemberTeamDto;
+
+import java.util.List;
+
+public interface MemberRepositoryCustom {   //사용자 정의 인터페이스
+
+    List<MemberTeamDto> search(MemberSearchParam param);
+
+}
--- queridsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ queridsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -1,7 +1,5 @@
 package study.querydsl.repository;
 
-
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,76 +7,52 @@ import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchParam;
 import study.querydsl.dto.MemberTeamDto;
-import study.querydsl.dto.QMemberTeamDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
+
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static study.querydsl.entity.QMember.member;
-import static study.querydsl.entity.QTeam.team;
 
 
 @SpringBootTest
 @Transactional
 @Commit
-class MemberJpaRepositoryTest {
+class MemberRepositoryTest {
 
     @Autowired
     EntityManager em;
 
     @Autowired
-    MemberJpaRepository memberJpaRepository;
+    MemberRepository memberRepository;
 
     @Test
     public void basicTest() {
 
+        //DB에 member 객체 저장
         Member member = new Member("member1", 10);
-
-        //member 객체를 DB에 저장
-        memberJpaRepository.save(member);
+        memberRepository.save(member);
 
         //id를 통해 member 객체 조회
-        Member findMember = memberJpaRepository.findById(member.getId()).get();
+        Member findMember = memberRepository.findById(member.getId()).get();
         assertThat(findMember).isEqualTo(member);
 
         //전체 member 객체 조회
-        List<Member> result1 = memberJpaRepository.findAll();
+        List<Member> result1 = memberRepository.findAll();
         assertThat(result1).containsExactly(member);
 
         //username을 통해 member 객체 조회
-        List<Member> result2 = memberJpaRepository.findByUsername("member1");
+        List<Member> result2 = memberRepository.findByUsername("member1");
         assertThat(result2).containsExactly(member);
 
     }
 
 
 
-    @Test
-    public void basicQuerydslTest() {
 
-        Member member = new Member("member1", 10);
-
-        //member 객체를 DB에 저장
-        memberJpaRepository.save(member);
-
-        //id를 통해 member 객체 조회
-        Member findMember = memberJpaRepository.findById(member.getId()).get();
-        assertThat(findMember).isEqualTo(member);
-
-        //(querydsl 사용) 전체 member 객체 조회
-        List<Member> result1 = memberJpaRepository.findAll_Querydsl();
-        assertThat(result1).containsExactly(member);
-
-        //(querydsl 사용) username을 통해 member 객체 조회
-        List<Member> result2 = memberJpaRepository.findByUsername_Querydsl("member1");
-        assertThat(result2).containsExactly(member);
-    }
-
-
-    //BooleanBuilder로 조건을 주어 member 객체를 조회하는 테스트
+    //where절에 메서드(파라미터)를 사용해 조건을 주어 member 객체를 조회하는 테스트
     @Test
     public void searchTest() {
 
@@ -98,25 +72,18 @@ class MemberJpaRepositoryTest {
         em.persist(member3);
         em.persist(member4);
 
-        //BooleanBuilder애 사용할 조건값 셋팅
+        //where절 함수(파라미터)에 사용할 조건값 셋팅
         MemberSearchParam param = new MemberSearchParam();
         param.setAgeGoe(35);   //나이 >= 35
         param.setAgeLoe(40);   //나이 <= 40
         param.setTeamName("teamB");
 
-        //param 조건을  BooleanBuilder에 넣어 Member 객체를 select한 결과를 MemberTeamDto객체로 반환
-        List<MemberTeamDto> result = memberJpaRepository.search(param);  //searchByBuilder(param);
+        //param 조건을  where절 함수(파라미터)에 넣어 Member 객체를 select한 결과를 MemberTeamDto객체로 반환
+        List<MemberTeamDto> result = memberRepository.search(param);  //함수(param);
 
         assertThat(result).extracting("username").containsExactly("member4");
 
     }
-
-
-
-
-
-
-
 
 
 
