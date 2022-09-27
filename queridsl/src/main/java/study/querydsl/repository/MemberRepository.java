b9ecbd4b01901253f9fd3dac679eb6ee5637399a
gusdn7142 <gusdnchl7144@naver.com>
2022-09-27 15:49:22 +0900

    Querydsl Study
    QuerydslPredicateExecutor Interface - findALL() Method Test

--- queridsl/src/main/java/study/querydsl/repository/MemberRepository.java
+++ queridsl/src/main/java/study/querydsl/repository/MemberRepository.java
@@ -2,12 +2,14 @@ package study.querydsl.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import study.querydsl.entity.Member;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long>,
-        MemberRepositoryCustom {
+        MemberRepositoryCustom,
+        QuerydslPredicateExecutor<Member> {
 
     List<Member> findByUsername(String username);   //select m from Member m where m.username=?
 
