8880606b7ade44c5a8dc3a612c64385b1f49707a
gusdn7142 <gusdnchl7144@naver.com>
2023-01-17 10:01:20 +0900

    Spring DB2 Study
    실습) outerTxOn_fail() : 회원정보와 로그정보를 등록⋅조회하는 서비스 개발5 (Service와 Repository 계층 모두에 @Transactional 적용) - 롤백 상황

--- springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
@@ -107,6 +108,26 @@ class MemberServiceTest {
     }
 
 
+
+    //테스트5 - 회원정보 등록시 커밋, 로그정보 등록시 롤백으로 전체 롤백 상황 (MemberService과 MemberRepository과 LogRepository에 @Transactional 적용)
+    /**
+     * MemberService @Transactional:ON
+     * MemberRepository @Transactional:ON
+     * LogRepository @Transactional:ON Exception
+     */
+    @Test
+    void outerTxOn_fail() {
+
+        //given
+        String username = "로그예외_outerTxOn_fail";   //username 필드에 값 입력
+
+        //when
+        assertThatThrownBy(() -> memberService.joinV1(username)).isInstanceOf(RuntimeException.class);   //회원 등록 & 로그 등록(+RuntimeException예외 발생)
+
+        //then: 모든 데이터가 롤백된다.
+        assertTrue(memberRepository.find(username).isEmpty());  //username을 통해 회원 조회.. 비어있음.
+        assertTrue(logRepository.find(username).isEmpty());     //username(==message필드값)을 통해 로그 조회.. 비어있음.
+    }
 
 
 
